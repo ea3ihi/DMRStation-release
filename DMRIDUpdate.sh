9bdb502bbfc8fb47b949df71c30c269b69f928a0
David <ea3ihi@gmail.com>
2020-06-06 15:56:13 +0200

    Added c2server. Lock button

--- DMRIDUpdate.sh
+++ DMRIDUpdate.sh
@@ -62,12 +62,6 @@ DMRFILEBACKUP=1
 #
 ###############################################################################
 
-# Check we are root
-if [ "$(id -u)" != "0" ]
-then
-        echo "This script must be run as root" 1>&2
-        exit 1
-fi
 
 # Create backup of old file
 if [ -f ${DMRIDFILE} ]
--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 ./md380-emu -S 2460 &
+./c2server -S 2470 &
 ./DMRstation
